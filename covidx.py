2f85ae24e56c0d6ecbbb5e0bb49442ddc0f00a8e
Irving Vasquez <jirvingvg@gmail.com>
2020-04-09 16:48:09 -0500

    resnet is training

--- covidx.py
+++ covidx.py
@@ -4,6 +4,8 @@ import pandas as pd
 import torch
 from skimage import io, transform
 
+from PIL import Image
+
 class COVIDxDataset(Dataset):
     def __init__(self, txt_frame_file, images_path, transform=None):
         """
@@ -18,7 +20,16 @@ class COVIDxDataset(Dataset):
         #self.landmarks_frame = pd.read_csv(csv_file)
         self.root_dir = images_path
         self.transform = transform
+        self.class2id = test_count = {'normal': 0, 'pneumonia': 1, 'COVID-19': 2}
+        self.id2class = {0: 'normal', 1: 'pneumonia', 2: 'COVID-19'}
         
+        
+    def pil_loader(self, path):
+        with open(path, 'rb') as f:
+            img = Image.open(f)
+            return img.convert('RGB')
+        
+    
     def __len__(self):
         return len(self.covidx_frame)
     
@@ -26,17 +37,23 @@ class COVIDxDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_name = os.path.join(self.root_dir,
+        image_path = os.path.join(self.root_dir,
                                 self.covidx_frame.iloc[idx, 1])
         #print(img_name)
-        image = io.imread(img_name)
+        
+        image = self.pil_loader(image_path)
+        #image = io.imread(img_name)
+        
+        
         label = self.covidx_frame.iloc[idx, 2]
         #landmarks = np.array([landmarks])
         #landmarks = landmarks.astype('float').reshape(-1, 2)
-        sample = {'image': image, 'label': label}
+        #sample = {'image': image, 'label': label}
 
-        if self.transform:
-            sample = self.transform(sample)
+        if self.transform is not None:
+            image = self.transform(image)
+            
+        sample = (image, self.class2id[label])
 
         return sample
         
